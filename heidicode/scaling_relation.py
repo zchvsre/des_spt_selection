550f15464891a5d306ddebb7a1ffe987f01ecd67
Zhou Conghao <zchusre@gmail.com>
2022-07-17 17:08:48 -0700

    add new scaling relation class

---
+++ heidicode/scaling_relation.py
@@ -0,0 +1,39 @@
+import numpy as np
+from colossus.cosmology import cosmology
+
+
+class ScalingRelation(object):
+    def __init__():
+        h = 0.6726
+        params = {
+            'flat': True,
+            'H0': 67.26,
+            'Om0': 0.14212 / h**2,
+            'Ob0': 0.02222 / h**2,
+            'sigma8': 0.81,
+            'ns': 9.9652,
+            'w0': -1.0,
+            'Neff': 3.04
+        }
+        cosmo = cosmology.setCosmology('Abacus', params)
+        h_70 = cosmo.H0 / 70
+        h_100 = cosmo.H0 / 100
+
+        self.Mpiv_lam = 5E14
+        self.alpha_lam = 0.939
+        self.pi_lam = 4.25 - self.alpha_lam * np.log(
+            self.Mpiv_lam) + 0.15 * np.log(1.3 / 1.45)
+        self.scatter_lam = 0.36
+
+        #SZ scaling relation from Bocquet et al. 2019
+        self.Mpiv_SZ = 4.3E14 / 0.7
+        self.alpha_SZ = 1.519
+        self.pi_SZ = np.log(5.68) - 1.519 * np.log(
+            self.Mpiv_SZ) + 0.547 * np.log(cosmo.Ez(0.3) / cosmo.Ez(0.6))
+        self.scatter_SZ = 0.152
+
+        #Mwl scaling relation
+
+        self.alpha_Mwl = 1
+        self.pi_Mwl = 0
+        sefl.scatter_Mwl = 0.5
